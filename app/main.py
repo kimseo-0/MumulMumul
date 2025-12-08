5010b2ac94b82696a40f451e655b083cc7e0f508
sy <42965120+kimseo-0@users.noreply.github.com>
2025-12-08 22:03:27 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,15 +4,16 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.api.connection import router as connection_router
 from app.api.attendance import router as attendance_router
-from app.api.meeting import router as meeting_router
+# from app.api.meeting import router as meeting_router
 from app.api.team_chat import router as team_chat_router
 from app.core.schemas import init_db
 from app.core.db import engine
 from app.core import schemas
-from app.services.meeting.audio_processor import AudioProcessor
+# from app.services.meeting.audio_processor import AudioProcessor
 from app.api.curriculum import router as curriculum_router
 from app.api.user import router as user_router
 from app.api.learning_chatbot import router as learning_chatbot_router
+from app.api.camp import router as camp_router
 from app.core.mongodb import init_mongo, get_mongo_db
 from app.core.schemas import init_db
 from app.config import SQLITE_URL
@@ -31,10 +32,10 @@ def create_app() -> FastAPI:
         except Exception:
             print("DB 마이그레이션 실패")
 
-        # whisper 모델 : 앱 시작시 1회 로드
-        print("Initializing Whisper model...")
-        AudioProcessor.initialize_whisper("large")
-        print("Whisper model ready!")
+        # # whisper 모델 : 앱 시작시 1회 로드
+        # print("Initializing Whisper model...")
+        # # AudioProcessor.initialize_whisper("large")
+        # print("Whisper model ready!")
         
         yield   
 
@@ -49,8 +50,9 @@ app = create_app()
 # ------------------------------
 app.include_router(connection_router, prefix="/connection")
 app.include_router(attendance_router, prefix="/attendance")
-app.include_router(meeting_router, prefix="/meeting")
+# app.include_router(meeting_router, prefix="/meeting")
 app.include_router(curriculum_router, prefix="/curriculum")
 app.include_router(user_router, prefix="/user")
 app.include_router(learning_chatbot_router, prefix="/learning_chatbot")
 app.include_router(team_chat_router, prefix="/chat")
+app.include_router(camp_router, prefix="/camps")
